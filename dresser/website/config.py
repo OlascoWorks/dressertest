31185a02a00d79880dff092077872745a53c313e
OlascoWorks <113733610+OlascoWorks@users.noreply.github.com>
2023-07-30 17:09:06 -0700

    Added extra configs to config.py

--- dresser/website/config.py
+++ dresser/website/config.py
@@ -1,8 +1,13 @@
+import os
+from . import DB_NAME
+
 class Config:
  DEBUG = False
  DEVELOPMENT = False
  CSRF_ENABLED = True
  ASSETS_DEBUG = False
+ SECRET_KEY = os.environ.get(SECRET_KEY)
+ SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
 
 class ProductionConfig(Config):
  pass
@@ -12,3 +17,5 @@ class DevelopmentConfig(Config):
  DEVELOPMENT = True
  TEMPLATES_AUTO_RELOAD = True
  ASSETS_DEBUG = True
+ SECRET_KEY = 'thisisatestsecretkey'
+ SQLALCHEMY_DATABASE_URI = f'sqlite:///{DB_NAME}'
